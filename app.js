7be3aab321c346cc6e393275b6331014dc4205ee
Madss Calls <madscalls@gmail.com>
2025-11-23 19:15:18 -0500

    github testing

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
-const mainRouter = require('./routes/index');
+const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
 const app = express();
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 mongoose
-  .connect('mongodb://127.0.0.1:27017/wtwr_db')
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
   })
   .catch(console.error);
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/', mainRouter);
+app.use("/", mainRouter);
 app.use(routes);
 
 app.listen(PORT, () => {
