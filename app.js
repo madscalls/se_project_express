4c434050554453c6444975afe6480656ea740c63
Madss Calls <madscalls@gmail.com>
2025-12-03 00:40:30 -0500

    lint fix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect('mongodb://127.0.0.1:27017/wtwr_db')
   .then(() => {
-    console.log("Connected to DB");
+    console.log('Connected to DB');
   })
   .catch(console.error);
 
@@ -17,12 +17,12 @@ app.use(cors());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "690d6b2f7256ef523081eecb",
+    _id: '690d6b2f7256ef523081eecb',
   };
   next();
 });
 
-app.use("/", routes);
+app.use('/', routes);
 
 // start server when this file is run directly
 const PORT = process.env.PORT || 3001;
