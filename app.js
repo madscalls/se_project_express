6deade9cb9ad85ca87fc8dae766cd728f2b0a1aa
Madss Calls <madscalls@gmail.com>
2025-12-03 00:38:14 -0500

    test user

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/wtwr_db')
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log('Connected to DB');
+    console.log("Connected to DB");
   })
   .catch(console.error);
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/', routes);
+app.use((req, res, next) => {
+  req.user = {
+    _id: "690d6b2f7256ef523081eecb",
+  };
+  next();
+});
+
+app.use("/", routes);
 
 // start server when this file is run directly
 const PORT = process.env.PORT || 3001;
